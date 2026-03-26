f5b4406c97e868ee6dcb4909c209ca58679e12a2
Ishan Pande <ishancpande2012@gmail.com>
2026-03-26 22:37:57 +0530

    LIST

--- list.py
+++ list.py
@@ -1,12 +1,6 @@
-list=['x','x','y','y','z','z','w','w','u','u']
-if x is str in list and y is str in list and z is str in list and r is str in list and x and y and z is not repeated in list:
-    print("list is unique")
-i=0
-while i!=len(list):
-    if list[i] == list[i+1]:
-        list.pop(i)
-        list.pop(i)
-    i+=1
-
-else:
-    print('')
+list=['apple','banana','cherry','apple','banana','cherry']
+uniquelist=[]
+for item in list:
+    if item not in uniquelist:
+        uniquelist.append(item)
+print(uniquelist)
